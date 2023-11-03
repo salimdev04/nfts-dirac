9e866faeda71016187493b8f0a24ea754ef5fa4d
Salimdev <Salimdev04@gmail.com>
2023-11-03 07:23:50 +0100

    Dirac Nft marketplace init

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export const data_images = [
+  {
+    id: 0,
+    image: "/data/0.png",
+  },
+  {
+    id: 1,
+    image: "/data/1.png",
+  },
+  {
+    id: 2,
+    image: "/data/2.png",
+  },
+  {
+    id: 3,
+    image: "/data/3.png",
+  },
+];
